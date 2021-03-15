3d78f5971f8609097b8adb149cb24a1f7a99e0e3
ranguli <hello@joshmurphy.ca>
2021-03-15 19:09:19 -0230

    Update pyproject.toml and added banner

--- hashcheck/cli.py
+++ hashcheck/cli.py
@@ -4,6 +4,25 @@ import click
 from hashcheck import Hash
 from hashcheck.exceptions import InvalidHashException
 
+from termcolor import colored, cprint
+
+banner = """
+888                        888               888                        888
+888                        888               888                        888
+888                        888               888                        888
+88888b.   8888b.  .d8888b  88888b.   .d8888b 88888b.   .d88b.   .d8888b 888  888
+888 "88b     "88b 88K      888 "88b d88P"    888 "88b d8P  Y8b d88P"    888 .88P
+888  888 .d888888 "Y8888b. 888  888 888      888  888 88888888 888      888888K
+888  888 888  888      X88 888  888 Y88b.    888  888 Y8b.     Y88b.    888 "88b
+888  888 "Y888888  88888P' 888  888  "Y8888P 888  888  "Y8888   "Y8888P 888  888
+
+                                                                           0.1.0
+
+https://github.com/ranguli/hashcheck
+"""
+
+cprint(banner, "blue")
+
 
 @click.command()
 @click.argument("file_hash")
