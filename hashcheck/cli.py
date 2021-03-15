2142dedb8fa4ea3a16a9c0fad8d500dc3f6e46aa
ranguli <hello@joshmurphy.ca>
2021-03-15 21:07:38 -0230

    Add VirusTotal to CLI

--- hashcheck/cli.py
+++ hashcheck/cli.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 
 import click
+from termcolor import colored, cprint
+
 from hashcheck import Hash
 from hashcheck.exceptions import InvalidHashException
+from hashcheck.formatters import VirusTotalFormatter
 
-from termcolor import colored, cprint
 
 banner = """
 888                        888               888                        888
@@ -17,18 +19,38 @@ banner = """
 888  888 "Y888888  88888P' 888  888  "Y8888P 888  888  "Y8888   "Y8888P 888  888
 
                                                                            0.1.0
-
 https://github.com/ranguli/hashcheck
 """
 
-cprint(banner, "blue")
+heading_color = "blue"
 
+cprint(banner, heading_color)
 
 @click.command()
 @click.argument("file_hash")
 def run(file_hash):
+    print(f"Checking hash {file_hash}.\n")
+
     try:
         _hash = Hash(file_hash)
-        print(_hash)
+        _hash.check()
+
     except InvalidHashException as e:
         print(e)
+
+    hash_algorithm_heading = colored("[*] Hashing algorithm:", heading_color)
+    print(f"{hash_algorithm_heading} {_hash.hash_type}")
+
+    virustotal = _hash.reports.virustotal
+    virustotal_formatter = VirusTotalFormatter(virustotal)
+
+    # Return most popular AV providers
+    investigation_url_heading = colored("[*] VirusTotal URL:", heading_color)
+    print(f"{investigation_url_heading} {virustotal.investigation_url}")
+
+    # Make a pretty table of the results
+    cprint("[*] VirusTotal detections:", "blue")
+    print(virustotal_formatter.detections)
+
+    reputation_heading = colored("[*] VirusTotal reputation:", "blue")
+    print(f"{reputation_heading} {virustotal_formatter.reputation}")
